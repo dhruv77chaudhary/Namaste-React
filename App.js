59d8c109b1bd14469a816127466500f58919597c
Dhruv Chauhdary <dhruv77chaudhary@gmail.com>
2024-12-20 16:22:07 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"This is dhruv chaudhary"),React.createElement("h2",{},"this is a second heading")]));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
