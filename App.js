7b47a60a7e4ccd6a778ff4ffaa050b3d7b4f238f
Dhruv Chauhdary <dhruv77chaudhary@gmail.com>
2024-12-21 14:35:59 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"This is dhruv chaudhary"),React.createElement("h2",{},"this is a second heading")]));
+import React from "react";
+import ReactDOM from "react-dom/client";
+    import {createRoot} from 'react-dom/client';
+// react functional component
+
+const Ja= (<h1>This is js element</h1>);
+
+const Title = () => (
+    <h1 className="head" tabIndex="5">Namste react using JSX</h1>
+);
+
+const HeadingComponent = () => (
+    <div id="container">
+        {Ja}
+        <Title/>
+        <h1 className="heading">Namaste React Functional component</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
+
--- Readme.md
+++ Readme.md
@@ -1,1 +1,13 @@
 # namste javascript
+
+# parcel
+-Dev Build
+-local server
+-HMR Hot module Replacement
+-file watching algorithm 
+-caching - faster build
+-Image optimization
+-minifcation of file
+-compressing
+-consisting hashing 
+-differential bundling for older browser
